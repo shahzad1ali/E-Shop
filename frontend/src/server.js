723c97e5cced9e2e8948ed5d42109aa1e4f7f08b
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-13 13:00:48 +0500

    add vercel links

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,11 @@
- export const server = 'https://e-shop-virid.vercel.app/api/v2';
 
-export const backend_url = 'https://e-shop-virid.vercel.app';
+ export const server = 'https://e-shop-tutorial-alpha.vercel.app/api/v2';
+
+export const backend_url = 'https://e-shop-tutorial-alpha.vercel.app';
+
+//  export const server = 'https://e-shop-virid.vercel.app/api/v2';
+
+// export const backend_url = 'https://e-shop-virid.vercel.app';
 
 
 //  export const server = 'http://localhost:8000/api/v2';
